c6104b3fce9f3f01488890e39784952a1f15a1ca
user <test@gmail.com>
2023-11-10 10:58:03 +0300

    first commit

---
+++ app/config.py
@@ -0,0 +1,9 @@
+class Configuration(object):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Parol_999@localhost/istoki_api?auth_plugin=mysql_native_password&charset=utf8'
+    SECRET_KEY = 'my site its very mega secret!'
+
+    SECURITY_PASSWORD_SALT = 'salt'
+    # SECURITY_PASSWORD_HASH = 'plaintext'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
